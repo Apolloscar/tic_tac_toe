1884f300c093b3bc42cb2067f8c1509fcfa50bae
Apolloscar <ohernandez4869@gmail.com>
2022-08-10 20:21:05 -0700

    Pint game function made
    prints game where 0 is empty, 1 = x, 2 = O

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
 
+using namespace std;
+
+void printGame(int game[])
+{
+    for(int i = 0; i < 9; i++)
+    {
+        switch(game[i])
+        {
+        case 1:
+            cout << "X";
+            break;
+        case 2:
+            cout << "O";
+            break;
+        default:
+            cout << " ";
+            break;
+        }
+        switch(i)
+        {
+        case 0:
+        case 1:
+        case 3:
+        case 4:
+        case 6:
+        case 7:
+            cout << " | ";
+            break;
+        case 2:
+        case 5:
+            cout << "\n---------\n";
+                 break;
+        default:
+            cout << "\n";
+            break;
+        }
+    }
+    return;
+}
+
+int main() {
+    int game[9];
+
+    printGame(game);
+    return 0;
+}
