9056bc58e237dc90856b228e6855cae55de7a5eb
Apolloscar <ohernandez4869@gmail.com>
2022-08-14 20:11:00 -0700

    easy game works

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+
 using namespace std;
+
+int checkWin(int game[])
+{
+    if(game[4] != 0)
+    {
+        if(game[4] == game[0] && game[4] == game[8])
+            return game[4];
+        if(game[4] == game[2] && game[4] == game[6])
+            return game[4];
+        if(game[4] == game[1] && game[4] == game[7])
+            return game[4];
+        if(game[4] == game[3] && game[4] == game[5])
+            return game[4];
+    }
+    if(game[0] != 0)
+    {
+        if(game[0] == game[3] && game[0] == game[6])
+            return game[0];
+        if(game[0] == game[1] && game[0] == game[2])
+            return game[0];
+    }
+    if(game[8] != 0)
+    {
+        if(game[8] == game[2] && game[8] == game[5])
+            return game[8];
+        if(game[8] == game[6] && game[8] == game[7])
+            return game[8];
+    }
+    return 0;
+}
+
 
 void printGame(int game[])
 {
@@ -40,13 +72,69 @@ void printGame(int game[])
     return;
 }
 
-int main() {
-    int game[9];
-    bool con = 1;
+void easyGame()
+{
+    int turn = 1;
+    bool play = 1;
+    int game[] = {0, 0, 0, 0,0, 0, 0, 0, 0};
 
+    int inp;
+
+    while(play && turn < 10)
+    {
+        printGame(game);
+        cout << "Select square 1-9: ";
+        cin >> inp;
+
+        inp--;
+
+        system("cls");
+        if(inp >= 0 && inp <= 8 && game[inp] == 0)
+        {
+            game[inp] = 1;
+            int winner = checkWin(game);
+            if(winner)
+            {
+                printGame(game);
+                cout << "X wins\n";
+                system("pause");
+                system("cls");
+                return;
+            }
+
+            turn++;
+
+            int choice = rand() %9;
+            while(game[choice] != 0 && turn <=9)
+            {
+                choice = rand() %9;
+            }
+            if(turn <=9)
+            {
+                game[choice] = 2;
+                winner = checkWin(game);
+                if(winner == 2)
+                {
+                    printGame(game);
+                    cout << "O wins\n";
+                    system("pause");
+                    system("cls");
+                    return;
+                }
+                turn++;
+            }
+        }
+    }
+}
+
+
+int main() {
+
+    bool con = 1;
     while(con)
     {
         int inp;
+
         cout << "Tic Tac Toe\n\n"
              << "Label of spaces are:\n\n"
              << "1 | 2 | 3\n"
@@ -60,14 +148,22 @@ int main() {
              << "3. Impossible\n"
              << "4. Quit\n\n"
              << "Enter option #: ";
+
         cin >> inp;
 
         system("cls");
 
-        if(inp == 4)
+        switch(inp)
         {
-            cout << "Thanks for playing";
+        case 1:
+            easyGame();
+            break;
+        case 4:
+            cout << "Thanks for playing\n";
             con = 0;
+            break;
+        default:
+            break;
         }
 
     }
